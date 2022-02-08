f4b9943a0ba5d0e94eeff20a6b6abe1f0dfb3775
Frankie <1426203851@qq.com>
2022-02-08 14:09:19 +0800

    chore: update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-
-  plugins: [reactRefresh()],
+  plugins: [react()],
 
   css: {
     modules: {
